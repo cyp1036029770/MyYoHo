7dfcb848e266946caa71adc4d9b74140714d73e7
cyp1036029770 <qq1036029770>
2016-08-29 15:20:58 +0800

    ssss

--- app/src/main/java/com/example/chenyunpeng/myyoho/fragment/FeileiFragment.java
+++ app/src/main/java/com/example/chenyunpeng/myyoho/fragment/FeileiFragment.java
@@ -46,7 +46,7 @@ public class FeileiFragment extends BaseFragment {
         fragmentList = new ArrayList<>();
         fragmentList.add(new PinLeiFragment());
         fragmentList.add(new PinpaiFragment());
-        fragmentList.add(new GuanzhuFragment());
+        fragmentList.add(new PinLeiFragment());
         FragmentManager manager = getFragmentManager();
         FeileiPagerAdapter adapter = new FeileiPagerAdapter(manager, titleList, fragmentList);
         viewpagerFeilei.setAdapter(adapter);
